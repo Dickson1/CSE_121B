47b41fe9ee98cc3a45b0f35e1bf80d1807de5bb0
Dickson1 <64061194+Dickson1@users.noreply.github.com>
2024-01-23 14:43:51 +0300

    Delete week3 directory

--- week3/js.js
+++
@@ -1,5 +0,0 @@
-const steps = ["one", "two", "three"];
-const stepsHtml = steps.map(function (step) {
-    return `<li>${step}</li>`;
-});
-document.getElementById("myList").innerHTML = stepsHtml.join();
